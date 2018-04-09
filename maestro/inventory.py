32ff0aad0749f266bfbf5e03367d9299e1b00bca
PedrosWits <ppintodasilva@gmail.com>
2018-04-09 15:40:30 +0100

    Fixed generated inventory: was missing definition of non-leaf nodes

--- maestro/inventory.py
+++ maestro/inventory.py
@@ -3,7 +3,7 @@
 """
 """
 
-from group import Group, for_each_group_below
+from group import Group, for_each_group_below, get_leaves, get_roots, get_non_leaves
 
 """
 hello
@@ -17,28 +17,38 @@ def get_servers_inventory(group):
     return servers
 
 
-def get_group_inventory(group):
-    group_inventory = ["[{}.children]".format(group.name)]
+def get_leaf_children(leaf):
+    inventory = ["[{}.children]".format(leaf.name)]
 
-    for i in xrange(0, group.servers, 1):
-        group_inventory.append(group.get_server_name(i+1))
+    for i in xrange(0, leaf.servers, 1):
+        inventory.append(leaf.get_server_name(i+1))
 
-    return group_inventory
+    return inventory
+
+def get_parent_children(group):
+    inventory = ["[{}.children]".format(group.name)]
+
+    for child in group.children:
+        print(child.name)
+        inventory.append(child.name)
+
+    return inventory
 
 
-def gen_inventory(roots):
+def gen_inventory(groups):
     ini_inventory = []
 
-    for_each_group_below(
-        groups = roots,
-        method = lambda group:
-            ini_inventory.append("\n".join(get_servers_inventory(group)))
-    )
+    leaves = get_leaves(groups)
 
-    for_each_group_below(
-        groups = roots,
-        method = lambda group:
-            ini_inventory.append("\n".join(get_group_inventory(group)))
-    )
+    for leaf in leaves:
+        ini_inventory.append("\n".join(get_servers_inventory(leaf)))
+        ini_inventory.append("\n".join(get_leaf_children(leaf)))
+
+    non_leaves = get_non_leaves(groups)
+
+    for non_leaf in non_leaves:
+        ini_inventory.append("\n".join(get_parent_children(non_leaf)))
+
+    ini_inventory.append("")
 
     return "\n\n".join(ini_inventory)
--- maestro/tests/test_gen_inventory.py
+++ maestro/tests/test_gen_inventory.py
@@ -12,210 +12,27 @@ config = \
     webservers:
       shiny: 1
       nginx: 1
-      other: 2
 
     databases:
       sql: 1
       other: 5
 
     generic:
-        other: 5
         windows:
             xp: 5
             seven: 5
-            other: 5
 
-    computing:
-      other: 7
+    computing: 7
     """
-
-def test_use_of_other_complex():
-
-    yaml_dict = yaml.safe_load(config)
-
-    groups = read_groups(yaml_dict, dict(), None)
-
-    assert groups["webservers"].servers == 2
-    assert groups["databases"].servers == 5
-    assert groups["computing"].servers == 7
-    assert groups["windows"].servers == 5
-    assert groups["generic"].servers == 5
-
-
-def test_for_each_group_above():
-    # Order of elements is not kept
-    yaml_dict = yaml.safe_load(config)
-    groups = read_groups(yaml_dict, dict(), None)
-
-    roots = get_roots(groups)
-
-    assert len(roots) == 4
-
-    # Need to sort alphabetically
-    roots.sort(key = lambda x: x.name)
-
-    # As the order of elements when yaml.safe_load is called
-    # is not kept, we can't do this
-    # expected_names = ["computing",
-    #                   "databases",
-    #                   "sql",
-    #                   "generic",
-    #                   "windows",
-    #                   "xp",
-    #                   "seven",
-    #                   "webservers",
-    #                   "shiny",
-    #                   "nginx"]
-    expected_names = [
-        "computing",
-        "databases",
-        "databases",
-        "generic",
-        "generic",
-        "windows",
-        "windows",
-        "webservers",
-        "webservers",
-        "webservers"]
-
-    names = []
-
-    for_each_group_below(
-        groups = roots,
-        method = lambda x:
-            names.append(x.name)
-            if x.isRoot()
-            else
-            names.append(x.parent.name))
-
-    assert expected_names == names
-
-
-def test_for_each_group_below():
-    # Order of elements is not kept
-    yaml_dict = yaml.safe_load(config)
-    groups = read_groups(yaml_dict, dict(), None)
-
-    leaves = get_leaves(groups)
-
-    assert len(leaves) == 6
-
-    # Need to sort alphabetically
-    leaves.sort(key = lambda x: x.name)
-
-    # In this case, parents with multiple leaves will execute once once per each children. This is not a bug, but intended. The objective of the function is not to execute once and only once on each node, but for a given node, execute upwards or downwards (at least for now).
-
-    expected_names = [
-        "computing", # computing leaf
-        # ---
-        "webservers", # nginx leaf
-        "webservers", # webserver
-        # --
-        "windows", # seven leaf
-        "generic", # windows
-        "generic", # generic
-        # ---
-        "webservers", # shiny leaf
-        "webservers", # webservers
-        # ---
-        "databases", # sql leaf
-        "databases", # databases
-        # ---
-        "windows", # xp leaf
-        "generic", # windows
-        "generic"] # generic
-
-    names_0 = []
-    names_1 = []
-    names_2 = []
-    names_3 = []
-    names_4 = []
-    names_5 = []
-
-    for_each_group_above(
-        group = leaves[0], # computing
-        method = lambda x:
-            names_0.append(x.name)
-            if x.isRoot()
-            else
-            names_0.append(x.parent.name))
-
-    assert expected_names[0:1] == names_0
-
-    
-    for_each_group_above(
-        group = leaves[1], # nginx
-        method = lambda x:
-            names_1.append(x.name)
-            if x.isRoot()
-            else
-            names_1.append(x.parent.name))
-
-    assert expected_names[1:3] == names_1
-
-    for_each_group_above(
-        group = leaves[2], # seven
-        method = lambda x:
-            names_2.append(x.name)
-            if x.isRoot()
-            else
-            names_2.append(x.parent.name))
-
-    assert expected_names[3:6] == names_2
-
-    for_each_group_above(
-        group = leaves[3], # shiny
-        method = lambda x:
-            names_3.append(x.name)
-            if x.isRoot()
-            else
-            names_3.append(x.parent.name))
-
-    assert expected_names[6:8] == names_3
-
-    for_each_group_above(
-        group = leaves[4], # sql
-        method = lambda x:
-            names_4.append(x.name)
-            if x.isRoot()
-            else
-            names_4.append(x.parent.name))
-
-    assert expected_names[8:10] == names_4
-
-    for_each_group_above(
-        group = leaves[5], # xp
-        method = lambda x:
-            names_5.append(x.name)
-            if x.isRoot()
-            else
-            names_5.append(x.parent.name))
-
-    assert expected_names[10:13] == names_5
-
 
 def test_gen_inventory():
     yaml_dict = yaml.safe_load(config)
     groups = read_groups(yaml_dict, dict(), None)
 
-    roots = get_roots(groups)
-
-    inventory = gen_inventory(roots)
+    inventory = gen_inventory(groups)
 
     assert inventory  == \
-"""[generic-001]
-[generic-002]
-[generic-003]
-[generic-004]
-[generic-005]
-
-[generic-windows-001]
-[generic-windows-002]
-[generic-windows-003]
-[generic-windows-004]
-[generic-windows-005]
-
-[windows-xp-001]
+"""[windows-xp-001]
 [windows-xp-002]
 [windows-xp-003]
 [windows-xp-004]
@@ -235,34 +52,17 @@ def test_gen_inventory():
 [computing-006]
 [computing-007]
 
-[webservers-001]
-[webservers-002]
-
 [webservers-nginx-001]
 
 [webservers-shiny-001]
 
-[databases-001]
-[databases-002]
-[databases-003]
-[databases-004]
-[databases-005]
+[databases-other-001]
+[databases-other-002]
+[databases-other-003]
+[databases-other-004]
+[databases-other-005]
 
 [databases-sql-001]
-
-[generic.children]
-generic-001
-generic-002
-generic-003
-generic-004
-generic-005
-
-[windows.children]
-generic-windows-001
-generic-windows-002
-generic-windows-003
-generic-windows-004
-generic-windows-005
 
 [xp.children]
 windows-xp-001
@@ -287,10 +87,6 @@ computing-005
 computing-006
 computing-007
 
-[webservers.children]
-webservers-001
-webservers-002
-
 [nginx.children]
 webservers-nginx-001
 
@@ -305,4 +101,15 @@ databases-004
 databases-005
 
 [sql.children]
-databases-sql-001"""
+databases-sql-001
+
+[webservers.children]
+nginx
+shiny
+
+[generic.children]
+windows
+
+[windows.children]
+xp
+seven"""
