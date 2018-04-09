0f3ef063ca5a5b6f52ce40537ac0eec22f7973b4
PedrosWits <ppintodasilva@gmail.com>
2018-04-09 15:52:04 +0100

    Fixed generate inventory test

--- maestro/tests/test_gen_inventory.py
+++ maestro/tests/test_gen_inventory.py
@@ -9,20 +9,15 @@ from maestro.group import get_roots, for_each_group_below, get_leaves, for_each_
 
 config = \
     """
-    webservers:
-      shiny: 1
-      nginx: 1
-
     databases:
       sql: 1
-      other: 5
+      neo4j-mongo: 1
 
-    generic:
-        windows:
-            xp: 5
-            seven: 5
+    webservers: 2
 
-    computing: 7
+    computing:
+      spark: 7
+      other: 1
     """
 
 def test_gen_inventory():
@@ -32,84 +27,51 @@ def test_gen_inventory():
     inventory = gen_inventory(groups)
 
     assert inventory  == \
-"""[windows-xp-001]
-[windows-xp-002]
-[windows-xp-003]
-[windows-xp-004]
-[windows-xp-005]
+"""[databases-neo4j-mongo-001]
 
-[windows-seven-001]
-[windows-seven-002]
-[windows-seven-003]
-[windows-seven-004]
-[windows-seven-005]
+[neo4j-mongo.children]
+databases-neo4j-mongo-001
 
-[computing-001]
-[computing-002]
-[computing-003]
-[computing-004]
-[computing-005]
-[computing-006]
-[computing-007]
+[webservers-001]
+[webservers-002]
 
-[webservers-nginx-001]
-
-[webservers-shiny-001]
-
-[databases-other-001]
-[databases-other-002]
-[databases-other-003]
-[databases-other-004]
-[databases-other-005]
+[webservers.children]
+webservers-001
+webservers-002
 
 [databases-sql-001]
-
-[xp.children]
-windows-xp-001
-windows-xp-002
-windows-xp-003
-windows-xp-004
-windows-xp-005
-
-[seven.children]
-windows-seven-001
-windows-seven-002
-windows-seven-003
-windows-seven-004
-windows-seven-005
-
-[computing.children]
-computing-001
-computing-002
-computing-003
-computing-004
-computing-005
-computing-006
-computing-007
-
-[nginx.children]
-webservers-nginx-001
-
-[shiny.children]
-webservers-shiny-001
-
-[databases.children]
-databases-001
-databases-002
-databases-003
-databases-004
-databases-005
 
 [sql.children]
 databases-sql-001
 
-[webservers.children]
-nginx
-shiny
+[computing-other-001]
 
-[generic.children]
-windows
+[computing-other.children]
+computing-other-001
 
-[windows.children]
-xp
-seven"""
+[computing-spark-001]
+[computing-spark-002]
+[computing-spark-003]
+[computing-spark-004]
+[computing-spark-005]
+[computing-spark-006]
+[computing-spark-007]
+
+[spark.children]
+computing-spark-001
+computing-spark-002
+computing-spark-003
+computing-spark-004
+computing-spark-005
+computing-spark-006
+computing-spark-007
+
+[computing.children]
+spark
+computing-other
+
+[databases.children]
+neo4j-mongo
+sql
+
+"""
