097e75890d0a02de1e845d7a6b8e5324fda36c69
PedrosWits <ppintodasilva@gmail.com>
2018-04-23 17:47:43 +0100

    Fixing previous commit: added the wrong test file

--- maestro/tests/test_read_roles.py
+++ maestro/tests/test_read_roles.py
@@ -4,29 +4,32 @@ import yaml
 import pytest
 from maestro.input import read_roles, read_groups
 
-groups = \
-    """
-    webservers:
-      shiny: 1
-      nginx: 1
-      other: 2
+@pytest.mark.skip(reason="I'm just an internal wrapper")
+def gen_groups():
 
-    databases:
-      sql: 1
-      other: 5
+    groups = \
+        """
+        webservers:
+          shiny: 1
+          nginx: 1
+          other: 2
 
-    generic:
-        other: 5
-        windows:
-            xp: 5
-            seven: 5
+        databases:
+          sql: 1
+          other: 5
+
+        generic:
             other: 5
+            windows:
+                xp: 5
+                seven: 5
+                other: 5
 
-    computing:
-      other: 7
-    """
+        computing:
+          other: 7
+        """
 
-groups = read_groups(yaml.safe_load(groups), dict(), None)
+    return read_groups(yaml.safe_load(groups), dict(), None)
 
 ###
 
@@ -41,7 +44,7 @@ def test_non_existing_group_name():
     yaml_dict = yaml.safe_load(config)
 
     with pytest.raises(ValueError):
-        read_roles(yaml_dict, groups)
+        read_roles(yaml_dict, gen_groups())
 
 ###
 
@@ -50,7 +53,7 @@ def test_bad_definition_of_role_variables():
     """
     webservers:
     databases:
-    jerome:
+    windows:
         - docker:
             - A
             - B
@@ -60,7 +63,7 @@ def test_bad_definition_of_role_variables():
     yaml_dict = yaml.safe_load(config)
 
     with pytest.raises(ValueError):
-        read_roles(yaml_dict, groups)
+        read_roles(yaml_dict, gen_groups())
 
 ###
 
@@ -69,14 +72,14 @@ def test_bad_definition_of_role():
     """
     webservers:
     databases:
-    jerome: 3
+    windows: 3
 
     """
 
     yaml_dict = yaml.safe_load(config)
 
     with pytest.raises(ValueError):
-        read_roles(yaml_dict, groups)
+        read_roles(yaml_dict, gen_groups())
 
 ###
 
@@ -85,7 +88,7 @@ def test_bad_definition_of_role_using_list_level_1():
     """
     webservers:
     databases:
-    jerome:
+    windows:
       - docker
       - derp
 
@@ -94,7 +97,7 @@ def test_bad_definition_of_role_using_list_level_1():
     yaml_dict = yaml.safe_load(config)
 
     with pytest.raises(ValueError):
-        read_roles(yaml_dict, groups)
+        read_roles(yaml_dict, gen_groups())
 
 ###
 
@@ -103,7 +106,7 @@ def test_bad_definition_of_role_using_list_level_2():
     """
     webservers:
     databases:
-    jerome:
+    windows:
       docker:
         - var1: "a"
         - var2: "b"
@@ -113,9 +116,24 @@ def test_bad_definition_of_role_using_list_level_2():
     yaml_dict = yaml.safe_load(config)
 
     with pytest.raises(ValueError):
-        read_roles(yaml_dict, groups)
+        read_roles(yaml_dict, gen_groups())
 
 ###
+
+def test_bad_definition_of_role_using_list_level_3():
+    config = \
+    """
+    webservers:
+    databases:
+    windows:
+      docker
+      derp:
+
+    """
+    with pytest.raises(yaml.scanner.ScannerError):
+        yaml_dict = yaml.safe_load(config)
+
+##
 
 def test_list_of_roles():
     config = \
@@ -148,7 +166,7 @@ def test_list_of_roles():
     """
 
     yaml_dict = yaml.safe_load(config)
-    boosted_groups = read_roles(yaml_dict, groups)
+    boosted_groups = read_roles(yaml_dict, gen_groups())
 
     assert len(boosted_groups["generic"].roles) == 0
     assert len(boosted_groups["webservers"].roles) == 0
@@ -181,5 +199,55 @@ def test_list_of_roles():
     assert boosted_groups["windows"].get_role("samba").variables ==  None
 
 
+def test_use_of_all():
+    config = \
+    """
+    webservers:
+    databases:
+    all:
+      docker:
+        username: JorgeJesus
+
+    """
+
+    yaml_dict = yaml.safe_load(config)
+    new_groups = read_roles(yaml_dict, gen_groups())
+
+    for groupname in new_groups.keys():
+        assert len(new_groups[groupname].roles) == 1
+        assert new_groups[groupname].has_role("docker")
+        assert new_groups[groupname].get_role("docker").variables == {"username" : "JorgeJesus"}
+
+
 def test_list_role_inheritance():
-    pass
+        config = \
+        """
+        databases:
+          docker:
+            username: JoaoPinto
+        all:
+          docker:
+            username: JorgeJesus
+
+        webservers:
+          docker:
+           username: LuisFigo
+        """
+
+        yaml_dict = yaml.safe_load(config)
+        new_groups = read_roles(yaml_dict, gen_groups())
+
+        assert new_groups["webservers"].get_role("docker").variables["username"] == "LuisFigo"
+        assert new_groups["shiny"].get_role("docker").variables["username"] == "LuisFigo"
+        assert new_groups["nginx"].get_role("docker").variables["username"] == "LuisFigo"
+        assert new_groups["webservers-other"].get_role("docker").variables["username"] == "LuisFigo"
+
+        assert new_groups["databases"].get_role("docker").variables["username"] == "JoaoPinto"
+        assert new_groups["sql"].get_role("docker").variables["username"] == "JoaoPinto"
+        assert new_groups["databases-other"].get_role("docker").variables["username"] == "JoaoPinto"
+
+        assert new_groups["generic"].get_role("docker").variables["username"] == "JorgeJesus"
+        assert new_groups["windows"].get_role("docker").variables["username"] == "JorgeJesus"
+        assert new_groups["seven"].get_role("docker").variables["username"] == "JorgeJesus"
+        assert new_groups["computing"].get_role("docker").variables["username"] == "JorgeJesus"
+        assert new_groups["computing-other"].get_role("docker").variables["username"] == "JorgeJesus"
